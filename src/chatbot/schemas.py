a11215679fabf4b5968b2c82b418937cb48ecad0
DarkLord <dipeshrathore122@gmail.com>
2025-02-28 04:01:54 +0530

    Updated API Schemas & Initial Server endpoints.

---
+++ src/chatbot/schemas.py
@@ -0,0 +1,228 @@
+"""Schema for the Agent Server."""
+
+from datetime import datetime
+from typing import Annotated, List
+from uuid import uuid4
+
+import bleach
+from pydantic import BaseModel, Field, StringConstraints, field_validator
+
+# List of fallback responses sent out for any Exceptions from /generate endpoint
+FALLBACK_RESPONSES = [
+    "Please try re-phrasing, I am likely having some trouble with that question.",
+    "I will get better with time, please try with a different question.",
+    "I wasn't able to process your input. Let's try something else.",
+    "Something went wrong. Could you try again in a few seconds with a different question?",
+    "Oops, that proved a tad difficult for me, can you retry with another question?",
+]
+
+
+class Message(BaseModel):
+    """Definition of the Chat Message type."""
+
+    role: str = Field(
+        description="Role for a message AI, User and System",
+        default="user",
+        max_length=256,
+        pattern=r"[\s\S]*",
+    )
+    content: str = Field(
+        description="The input query/prompt to the pipeline.",
+        default="Hello what can you do?",
+        max_length=131072,
+        pattern=r"[\s\S]*",
+    )
+
+    timestamp: str = Field(
+        default_factory=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+        description="Message Timestamp",
+    )
+
+    @field_validator("role")
+    def validate_role(cls, value):
+        """Field validator function to validate values of the field role"""
+        value = bleach.clean(value, strip=True)
+        valid_roles = {"user", "assistant", "system"}
+        if value.lower() not in valid_roles:
+            raise ValueError("Role must be one of 'user', 'assistant', or 'system'")
+        return value.lower()
+
+    @field_validator("content")
+    def sanitize_content(cls, v):
+        """Field validator function to santize user populated feilds from HTML"""
+        v = bleach.clean(v, strip=True)
+        if not v:  # Check for empty string
+            raise ValueError("Message content cannot be empty.")
+        return v
+
+    @field_validator("timestamp")
+    def sanitize_timestamp(cls, v):
+        """Field validator function to santize given timestamp"""
+        if not isinstance(v, (str, float)):
+            raise ValueError("Timestamp must be a string or float.")
+        elif isinstance(v, float):
+            try:
+                v = datetime.fromtimestamp(v).strftime("%Y-%m-%d %H:%M:%S.%f")
+            except ValueError:
+                raise ValueError("Invalid timestamp format. [invalid-float]")
+
+        elif isinstance(v, str):
+            try:
+                v = datetime.fromisoformat(v)
+            except ValueError:
+                raise ValueError("Timestamp must be in valid ISO format string.")
+
+        return v
+
+
+# Request Schemas
+class Prompt(BaseModel):
+    """Definition of the Prompt API data type."""
+
+    messages: List[Message] = Field(
+        ...,
+        description="A list of messages comprising the conversation so far. The roles of the messages must be alternating between user and assistant. The last input message should have role user. A message with the the system role is optional, and must be the very first message if it is present.",
+        max_length=10_000,
+    )
+
+    user_id: str = Field(description="A unique identifier representing your end-user.")
+    session_id: str = Field(
+        ...,
+        description="A unique identifier representing the session associated with the response.",
+    )
+
+
+class ChainResponseChoices(BaseModel):
+    """Definition of Chain response choices"""
+
+    index: int = Field(default=0, ge=0, le=255)
+    message: Message = Field(default=Message())
+    finish_reason: str = Field(default="", max_length=4096, pattern=r"[\s\S]*")
+
+
+class ChainResponse(BaseModel):
+    """Definition of Chain APIs resopnse data type"""
+
+    id: str = Field(default="", max_length=100000, pattern=r"[\s\S]*")
+    choices: List[ChainResponseChoices] = Field(default=[], max_length=256)
+    session_id: str = Field(
+        description="A unique identifier representing the session associated with the response.",
+    )
+
+
+class DocumentSearch(BaseModel):
+    """Definition of the DocumentSearch API data type."""
+
+    query: str = Field(
+        description="The content or keywords to search for within documents.",
+        max_length=131072,
+        pattern=r"[\s\S]*",
+        default="",
+    )
+    top_k: int = Field(
+        description="The maximum number of documents to return in the response.",
+        default=4,
+        ge=0,
+        le=25,
+    )
+
+
+class DocumentChunk(BaseModel):
+    """Represents a chunk of a document."""
+
+    content: str = Field(
+        description="The content of the document chunk.",
+        max_length=131072,
+        pattern=r"[\s\S]*",
+        default="",
+    )
+    filename: str = Field(
+        description="The name of the file the chunk belongs to.",
+        max_length=4096,
+        pattern=r"[\s\S]*",
+        default="",
+    )
+    score: float = Field(..., description="The relevance score of the chunk.")
+
+
+class DocumentSearchResponse(BaseModel):
+    """Represents a response from a document search."""
+
+    chunks: List[DocumentChunk] = Field(
+        ..., description="List of document chunks.", max_length=256
+    )
+
+
+class DocumentsResponse(BaseModel):
+    """Represents the response containing a list of documents."""
+
+    DocumentString: Annotated[
+        str, StringConstraints(max_length=131072, pattern=r"[\s\S]*")
+    ] = Field(description="List of filenames.", max_length=1000000, default="")
+
+
+class HealthResponse(BaseModel):
+    message: str = Field(max_length=4096, pattern=r"[\s\S]*", default="")
+
+
+class CreateThreadResponse(BaseModel):
+    session_id: str = Field(max_length=4096)
+
+
+class DeleteThreadResponse(BaseModel):
+    message: str = Field(max_length=4096, pattern=r"[\s\S]*", default="")
+
+
+class FeedbackRequest(BaseModel):
+    """Definition of the Feedback Request data type."""
+
+    feedback: float = Field(
+        ...,
+        description="A unique identifier representing your end-user.",
+        ge=-1.0,
+        le=1.0,
+    )
+    session_id: str = Field(
+        ...,
+        description="A unique identifier representing the session associated with the response.",
+    )
+
+
+class FeedbackResponse(BaseModel):
+    """Definition of the Feedback Request data type."""
+
+    message: str = Field(max_length=4096, pattern=r"[\s\S]*", default="")
+
+
+class GetSessionResponse(BaseModel):
+    session_id: str
+    user_id: str
+    conversation_history: List[Message]
+    last_conversation_time: str = "None"
+    start_conversation_time: str = "None"
+
+
+def fallback_response_generator(sentence: str, session_id: str = ""):
+    """Mock response generator to simulate streaming predefined fallback responses."""
+
+    # Simulate breaking the sentence into chunks (e.g., by word)
+    sentence_chunks = sentence.split()  # Split the sentence by words
+    resp_id = str(uuid4())  # unique response id for every query
+    # Send each chunk (word) in the response
+    for chunk in sentence_chunks:
+        chain_response = ChainResponse(session_id=session_id)
+        response_choice = ChainResponseChoices(
+            index=0, message=Message(role="assistant", content=f"{chunk} ")
+        )
+        chain_response.id = resp_id
+        chain_response.choices.append(response_choice)
+        yield "data: " + str(chain_response.model_dump()) + "\n\n"
+
+    # End with [DONE] response
+    chain_response = ChainResponse(session_id=session_id)
+    response_choice = ChainResponseChoices(
+        message=Message(role="assistant", content=" "), finish_reason="[DONE]"
+    )
+    chain_response.id = resp_id
+    chain_response.choices.append(response_choice)
+    yield "data: " + str(chain_response.model_dump()) + "\n\n"
---
+++ src/chatbot/server.py
@@ -0,0 +1,380 @@
+"""API Server for ChatBot Endpoints."""
+
+import asyncio
+import logging
+import os
+import random
+import re
+import time
+from contextlib import asynccontextmanager
+from traceback import print_exc
+from uuid import uuid4
+
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import HTTPException, RequestValidationError
+from fastapi.responses import JSONResponse, StreamingResponse
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+
+from src.chatbot.cache.session_manager import SessionManager
+from src.chatbot.datastore.datastore import Datastore
+from src.chatbot.main import CompiledStateGraph, get_agent
+from src.chatbot.schemas import (  # EndSessionResponse,; FeedbackResponse,
+    FALLBACK_RESPONSES,
+    ChainResponse,
+    ChainResponseChoices,
+    CreateThreadResponse,
+    DeleteThreadResponse,
+    GetSessionResponse,
+    Message,
+    Prompt,
+    fallback_response_generator,
+)
+from src.chatbot.utils import get_async_pool
+
+logging.basicConfig(level=os.getenv("LOG_LEVEL", logging.INFO))
+logger = logging.getLogger(__name__)
+logger.info("Initializing procurement agent app...")
+
+# Initialize app
+session_manager: SessionManager
+datastore: Datastore
+agent: CompiledStateGraph
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan of the FastAPI app."""
+
+    # Load required resources
+    global session_manager
+    global datastore
+    global agent
+
+    async_pool = get_async_pool()
+    await async_pool.open(wait=True)
+    print("✌️ Connections got!!!")
+
+    session_manager = SessionManager()
+    datastore = Datastore()
+    agent = await get_agent()
+
+    await datastore.database.init_script()
+
+    yield
+    # Clean up the resources
+    await async_pool.close()
+
+
+# FastAPI app
+app = FastAPI(title="Osho Chatbot API", lifespan=lifespan)
+
+
+# Routes
+@app.get("/healthz")
+def healthz():
+    """Health Check for Chatbot Server."""
+
+    return {"status": "ok", "message": "ChatBot API service is healthy"}
+
+
+# Other db testing routes
+@app.get(
+    "/create_thread",
+    tags=["Thread Management"],
+    response_model=CreateThreadResponse,
+    responses={
+        500: {
+            "description": "Internal Server Error",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Internal server error occurred"}
+                }
+            },
+        }
+    },
+)
+async def create_thread() -> CreateThreadResponse:
+    """Create a new conversation thread."""
+
+    # Try for fix number of time, if no unique session_id is found raise Error
+    for _ in range(5):
+        session_id = str(uuid4())
+
+        # Ensure session_id created does not exist in cache
+        if not session_manager.is_valid_thread(session_id):
+            # Ensure session_id created does not exist in datastore (permanenet store like postgres)
+            if not await datastore.is_valid_thread(session_id):
+                # Create a session on cache for validation
+                if session_manager.create_conversation_thread(session_id):
+                    await datastore.save_update_thread(
+                        thread_id=session_id,
+                        **session_manager.get_thread_info(session_id),
+                    )
+                    return CreateThreadResponse(session_id=session_id)
+
+    raise HTTPException(status_code=500, detail="Unable to generate session_id")
+
+
+@app.post(
+    "/generate",
+    tags=["Inference"],
+    response_model=ChainResponse,
+    responses={
+        500: {
+            "description": "Internal Server Error",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Internal server error occurred"}
+                }
+            },
+        }
+    },
+)
+async def generate_answer(request: Request, prompt: Prompt) -> StreamingResponse:
+    """Generate and stream the response to the provided prompt."""
+
+    logger.info(f"Input at /generate endpoint of Agent: {prompt.dict()}")
+
+    try:
+        user_query_timestamp = time.time()
+
+        # Handle invalid session id
+        if not session_manager.is_valid_thread(prompt.session_id):
+            logger.error(
+                f"No session_id created {prompt.session_id}. Please create session id before generate request."
+            )
+            print_exc()
+            return StreamingResponse(
+                fallback_response_generator(
+                    sentence=random.choice(FALLBACK_RESPONSES),
+                    session_id=prompt.session_id,
+                ),
+                media_type="text/event-stream",
+            )
+        chat_history = prompt.messages
+        # The last user message will be the query for the rag or llm chain
+        last_user_message = next(
+            (
+                message.content
+                for message in reversed(chat_history)
+                if message.role == "user"
+            ),
+            None,
+        )
+
+        # Normalize the last user input and remove non-ascii characters
+        last_user_message = re.sub(
+            r"[^\x00-\x7F]+", "", str(last_user_message)
+        )  # Remove all non-ascii characters
+        last_user_message = re.sub(
+            r"[\u2122\u00AE]", "", last_user_message
+        )  # Remove standard trademark and copyright symbols
+        last_user_message = last_user_message.replace("~", "-")
+        logger.info(f"Normalized user input: {last_user_message}")
+
+        # Keep copy of unmodified query to store in db
+        user_query = last_user_message
+
+        async def response_generator():
+            resp_id = str(uuid4())
+            resp_str = ""
+
+            chain_response = None
+            # Mock agent response
+            for content in fallback_response_generator(
+                sentence=random.choice(FALLBACK_RESPONSES), session_id=prompt.session_id
+            ):
+                resp_str += content
+
+                if content:
+                    chain_response = ChainResponse(session_id=prompt.session_id)
+                    response_choice = ChainResponseChoices(
+                        index=0,
+                        message=Message(role="assistant", content=content),
+                    )
+                    chain_response.id = resp_id
+                    chain_response.session_id = prompt.session_id
+                    chain_response.choices.append(response_choice)
+                    logger.debug(response_choice)
+
+                    yield "data: " + str(chain_response.model_dump()) + "\n\n"
+
+                chain_response = ChainResponse(session_id=prompt.session_id)
+            # Initialize content with space to overwrite default response
+            response_choice = ChainResponseChoices(
+                index=0,
+                message=Message(role="assistant", content=" "),
+                finish_reason="[DONE]",
+            )
+
+            logger.info(
+                f"Conversation saved:\nSession ID: {prompt.session_id}\nQuery: {last_user_message}\nResponse: {resp_str}"
+            )
+            logger.info("Saving to both cache and pg database")
+
+            # Saving to cache & Database
+            response_timestamp = time.time()
+            # Cache should fetch thread from db first.
+            session_manager.update_conversation_thread(
+                prompt.session_id,
+                prompt.user_id or "",
+                [
+                    {
+                        "role": "user",
+                        "content": user_query,
+                        "timestamp": f"{user_query_timestamp}",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": resp_str,
+                        "timestamp": f"{response_timestamp}",
+                    },
+                ],
+                last_conversation_time=user_query_timestamp,
+            )
+
+            # Can go to FastAPI's Background process.
+            await datastore.save_update_thread(
+                prompt.session_id,
+                prompt.user_id or "",
+                [
+                    {
+                        "role": "user",
+                        "content": user_query,
+                        "timestamp": f"{user_query_timestamp}",
+                    },
+                    {
+                        "role": "assistant",
+                        "content": resp_str,
+                        "timestamp": f"{response_timestamp}",
+                    },
+                ],
+                last_conversation_time=user_query_timestamp,
+            )
+
+            chain_response.id = resp_id
+            # chain_response.session_id = prompt.session_id
+            chain_response.choices.append(response_choice)
+            logger.debug(response_choice)
+            yield "data: " + str(chain_response.model_dump()) + "\n\n"
+
+        return StreamingResponse(response_generator(), media_type="text/event-stream")
+    # Catch any unhandled exceptions
+    except asyncio.CancelledError:
+        # Handle the cancellation gracefully
+        logger.error(
+            "Unhandled Server interruption before response completion. Details: {e}"
+        )
+        print_exc()
+        return StreamingResponse(
+            fallback_response_generator(
+                sentence=random.choice(FALLBACK_RESPONSES), session_id=prompt.session_id
+            ),
+            media_type="text/event-stream",
+        )
+    except Exception as e:
+        logger.error(f"Unhandled Error from /generate endpoint. Error details: {e}")
+        print_exc()
+        return StreamingResponse(
+            fallback_response_generator(
+                sentence=random.choice(FALLBACK_RESPONSES), session_id=prompt.session_id
+            ),
+            media_type="text/event-stream",
+        )
+
+
+# Get conversation
+@app.get(
+    "/get_thread_info",
+    tags=["Thread Management"],
+    response_model=GetSessionResponse,
+    responses={
+        500: {
+            "description": "Internal Server Error",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Internal server error occurred"}
+                }
+            },
+        }
+    },
+)
+async def get_thread_info(thread_id):
+    """Get conversation_thread info from cache or database."""
+    logger.info(f"Getting conversation for {thread_id}")
+    if not session_manager.is_valid_thread(thread_id):
+        if not await datastore.is_valid_thread(thread_id):
+            logger.info("No conversation found in session or database")
+            return HTTPException(404, detail="Session info not found")
+
+        session_info = await datastore.get_thread_info(thread_id)
+        if session_info:
+            session_manager.update_conversation_thread(
+                thread_id,
+                session_info["user_id"],
+                session_info["conversation_history"],
+            )
+
+        else:
+            logger.info("No conversation found in session or database")
+            return HTTPException(404, detail="Invalid Session info found!")
+
+    session_info = session_manager.get_thread_info(thread_id)
+    return GetSessionResponse(
+        session_id=thread_id,
+        user_id=session_info["user_id"],
+        conversation_history=session_info["conversation_history"],
+    )
+
+
+@app.delete(
+    "/delete_thread",
+    tags=["Thread Management"],
+    response_model=DeleteThreadResponse(),
+    responses={
+        500: {
+            "description": "Internal Server Error",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Internal server error occurred"}
+                }
+            },
+        }
+    },
+)
+async def delete_thread(thread_id):
+    """Delete conversation_thread from cache and database."""
+    logger.info(f"Deleting conversation for {thread_id}")
+    session_info = session_manager.get_thread_info(thread_id)
+    datastore_session_info = await datastore.get_thread_info(thread_id)
+
+    if not session_info or not datastore_session_info:
+        logger.info("No conversation found in session")
+        return DeleteThreadResponse(message="Session info not found")
+
+    logger.info(f"Deleting conversation for {thread_id} from cache")
+    session_manager.delete_conversation_thread(thread_id)
+
+    logger.info(f"Deleting conversation for {thread_id} in database")
+    await datastore.delete_conversation_thread(thread_id)
+
+    logger.info(f"Deleting checkpointer for {thread_id}")
+    # remove_state_from_checkpointer(session_id)
+    return DeleteThreadResponse(message="Session info deleted")
+
+
+# Other Exception handling
+@app.exception_handler(RequestValidationError)
+async def request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
+    """Request Validation Exception Handler"""
+
+    return JSONResponse(
+        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"detail": jsonable_encoder(exc.errors(), exclude={"input"})},
+    )
+
+
+logger.info("Procurement-Agent service initialized...")
